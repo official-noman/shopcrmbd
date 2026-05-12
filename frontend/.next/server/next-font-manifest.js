2f6c72235c3f82adc2886edca2139d5436b429a2
official-noman <official.aanoman@gmail.com>
2026-05-12 09:26:27 +0600

    Fix: Remove node_modules from git tracking

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/noman-mahmud/Noman/ShopCRM BD/frontend/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
